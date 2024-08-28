a587c9980e49a6b6e5089b8fabde0941a5d982b7
mathiashuygen <mathias.louis.huygen@vub.be>
2024-08-28 20:56:47 +0200

    added keymap to show list of buffers

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -11,6 +11,7 @@ return {
 				
 				vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 				vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+				vim.keymap.set('n', '<C-h>', builtin.buffers, {})
 				end
 
 }
